242dde8a81b39a896c33c9970f947651a1543367
Sumon400011 <sumonsm89@gmail.com>
2022-11-05 00:00:16 +0600

    dump lines removed

--- src/main.js
+++ src/main.js
@@ -3,13 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import jQuery from "jquery";
-// import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "popper.js";
 window.$ = window.jQuery = jQuery;
-
-// import "./assets/style.min.css";
-// import "./assets/theme.min.css";
 
 const app = createApp(App);
 
