9990f4eb0687f14e3c38b0524fcc093a41d65151
Sumon400011 <sumonsm89@gmail.com>
2022-11-04 16:46:55 +0600

    Education Section Added

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,17 +2,20 @@
   <main>
     <BannerComp />
     <AboutmeComp />
+    <EducationComp />
   </main>
 </template>
 
 <script>
 import BannerComp from "../components/HomeView/BannerComp.vue";
 import AboutmeComp from "../components/HomeView/AboutmeComp.vue";
+import EducationComp from "../components/HomeView/EducationComp.vue";
 export default {
   name: "HomePage",
   components: {
     BannerComp,
     AboutmeComp,
+    EducationComp,
   },
 };
 </script>
