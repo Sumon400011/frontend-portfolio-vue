3838317a7666a4a5885ed2196523f7beca61eca3
Sumon400011 <sumonsm89@gmail.com>
2022-11-04 16:39:59 +0600

    about me section added homepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,15 +1,18 @@
 <template>
   <main>
     <BannerComp />
+    <AboutmeComp />
   </main>
 </template>
 
 <script>
 import BannerComp from "../components/HomeView/BannerComp.vue";
+import AboutmeComp from "../components/HomeView/AboutmeComp.vue";
 export default {
   name: "HomePage",
   components: {
     BannerComp,
+    AboutmeComp,
   },
 };
 </script>
