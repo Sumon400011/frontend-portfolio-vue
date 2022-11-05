bb1c657f893d457afa8831689d632bb2402d5441
Sumon400011 <sumonsm89@gmail.com>
2022-11-05 13:43:51 +0600

    skills card component added

--- src/components/HomeView/SkillsComp.vue
+++ src/components/HomeView/SkillsComp.vue
@@ -10,81 +10,16 @@
           <h2 class="h1 text-styled title">Skills</h2>
         </div>
       </div>
-      <div class="row">
-        <div class="col-12 col-sm-12 col-md-6 col-lg-4 mb-32">
-          <div class="card">
-            <div class="view">
-              <i class="fa-brands fa-react fa-6x my-4" aria-hidden="true"></i>
-              <p class="card-title text-md weight-bold">
-                <strong>Front-End Development</strong>
-              </p>
-            </div>
-            <div class="card-body">
-              <ul class="mb-0">
-                <li>HTML, CSS, JavaScript & JQuery</li>
-                <li>Vue.js / React js</li>
-                <li>CSS Preprocessor - Sass</li>
-                <li>Wordpress - DIVI</li>
-                <li>Webflow</li>
-                <li>CSS Frameworks (Bootstrap)</li>
-                <li>Responsive Web Design</li>
-              </ul>
-            </div>
-            <div class="card-footer">&nbsp;</div>
-          </div>
-        </div>
-
-        <div class="col-12 col-sm-12 col-md-6 col-lg-4 mb-32">
-          <div class="card">
-            <div class="view">
-              <i class="fa-solid fa-brush fa-6x my-4" aria-hidden="true"></i>
-              <p class="card-title text-md weight-bold">
-                <strong>Graphics & UI Design</strong>
-              </p>
-            </div>
-            <div class="card-body">
-              <ul class="mb-0">
-                <li>Photoshop</li>
-                <li>Illustrator</li>
-                <li>Adobe Xd / Figma</li>
-                <li>Web UI design</li>
-                <li>Logo Design</li>
-                <li>Banner Design</li>
-                <li>Buisiness Card Design</li>
-              </ul>
-            </div>
-            <div class="card-footer">&nbsp;</div>
-          </div>
-        </div>
-
-        <div class="col-12 col-sm-12 col-md-6 col-lg-4 mb-32">
-          <div class="card">
-            <div class="view">
-              <i class="fa-solid fa-shapes fa-6x my-4" aria-hidden="true"></i>
-              <p class="card-title text-md weight-bold">
-                <strong>Others</strong>
-              </p>
-            </div>
-            <div class="card-body">
-              <ul class="mb-0">
-                <li>Git / Github</li>
-                <li>Email Marketing (Mailchimp)</li>
-                <li>Project Management with Redmine / Jira</li>
-                <li>Finding Bug and Fixing</li>
-                <li>Laravel and Node js (Beginner)</li>
-                <li>Css Frameworks - Tailwind / Materialize css</li>
-                <li>Node.js / Express.js</li>
-              </ul>
-            </div>
-            <div class="card-footer">&nbsp;</div>
-          </div>
-        </div>
-      </div>
+      <SkillCard />
     </div>
   </section>
 </template>
 <script>
+import SkillCard from "../Misc/SkillCard.vue";
 export default {
   name: "SkillsComp",
+  components: {
+    SkillCard,
+  },
 };
 </script>
---
+++ src/components/Misc/SkillCard.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="row">
+    <div
+      class="col-12 col-sm-12 col-md-6 col-lg-4 mb-32"
+      v-for="skill in skills"
+      :key="skill.id"
+    >
+      <div class="card">
+        <div class="view">
+          <span v-html="skill.icon"></span>
+          <p class="card-title text-md weight-bold">
+            <strong>{{ skill.name }}</strong>
+          </p>
+        </div>
+        <div class="card-body">
+          <ul class="mb-0">
+            <li v-for="(set, index) in skill.sets" :key="index">
+              {{ set.name }}
+            </li>
+          </ul>
+        </div>
+        <div class="card-footer">&nbsp;</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "SkillCard",
+  data: function () {
+    return {
+      skills: [
+        {
+          id: 1,
+          name: "frontend",
+          icon: "<i class='fa-brands fa-react fa-6x my-4' aria-hidden='true'></i>",
+          sets: [
+            { name: "HTML, CSS, JavaScript & JQuery" },
+            { name: "Vue.js" },
+            { name: "CSS Preprocessor - Sass" },
+            { name: "Wordpress - DIVI" },
+            { name: "Webflow" },
+            { name: "CSS Frameworks (Bootstrap)" },
+            { name: "Responsive Web Design" },
+            { name: "Nuxt.js (Beginner)" },
+          ],
+        },
+        {
+          id: 2,
+          name: "graphics",
+          icon: "<i class='fa-solid fa-brush fa-6x my-4' aria-hidden='true'></i>",
+          sets: [
+            { name: "Photoshop" },
+            { name: "Illustrator" },
+            { name: "Adobe Xd / Figma" },
+            { name: "Web UI design" },
+            { name: "Logo Design" },
+            { name: "Banner Design" },
+            { name: "Business Card Design" },
+            { name: "More" },
+          ],
+        },
+        {
+          id: 3,
+          name: "others",
+          icon: "<i class='fa-solid fa-shapes fa-6x my-4' aria-hidden='true'></i>",
+          sets: [
+            { name: "Git / Github" },
+            { name: "Email Marketing (Mailchimp)" },
+            { name: "Project Management with Redmine / Jira" },
+            { name: "Finding Bug and Fixing" },
+            { name: "Node js (Beginner)" },
+            { name: "React js (Beginner)" },
+            { name: "Css Frameworks - Tailwind / Materialize css" },
+          ],
+        },
+      ],
+    };
+  },
+};
+</script>
