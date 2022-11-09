327d372513239ced1a1c4a307f07df450f144268
Sumon400011 <sumonsm89@gmail.com>
2022-11-09 18:10:24 +0600

    vue gtag added for trackings

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import jQuery from "jquery";
+import VueGtag from "vue-gtag";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "popper.js";
 window.$ = window.jQuery = jQuery;
 
 const app = createApp(App);
-
+app.use(VueGtag, {
+  config: { id: "G-FML00HTN5B" },
+});
 app.use(store).use(router).mount("#app");
